1ea5f4111fc2617d662a5d73211203a8e3a8f498
anna <anna8g5@mail.ru>
2021-10-21 19:40:16 +0300

    Add new functions and classes

--- Lab6.py
+++ Lab6.py
@@ -1,11 +1,13 @@
 import pygame
 import random
+import yaml
 
 pygame.init()
 
 FPS = 10
-screen_color = (255, 255, 255)
-screen = pygame.display.set_mode((1200, 700))
+screen_color = (130, 219, 91)
+display_size = (1200, 700)
+screen = pygame.display.set_mode(display_size)
 screen.fill(screen_color)
 
 
@@ -15,11 +17,13 @@ class Ball:
         init method to class Ball
         '''
         max_velocity = 5
-        self.coords = [random.randint(0, 900), random.randint(0, 600)]  # coordinates of center
+        self.coords = [random.randint(0, 900), random.randint(0, 600)]
+        # coordinates of center
         self.color = (random.randint(0, 255), random.randint(0, 255),
                       random.randint(0, 255))  # color
         self.velocity = [random.randint(-max_velocity, max_velocity),
-                         random.randint(-max_velocity, max_velocity)]  # velocity
+                         random.randint(-max_velocity, max_velocity)]
+        # velocity
         self.r = random.randint(10, 40)  # radius
         self.life = True  # indicator of existence
 
@@ -29,18 +33,21 @@ class Ball:
         '''
         self.coords = [self.coords[0] + self.velocity[0],
                        self.coords[1] + self.velocity[1]]
+        # change coordinates
         self.reflection(pygame.display.get_surface().get_size())
 
     def reflection(self, BORDER_EDGERS):
         '''
         reflection from wall
         '''
-        if abs(self.coords[0] - 0) <= self.r  and self.velocity[0] < 0 or (
-            abs(self.coords[0] - BORDER_EDGERS[0]) <= self.r and  self.velocity[0] > 0):
+        wall_l = abs(self.coords[0] - 0) <= self.r
+        wall_r = abs(self.coords[0] - BORDER_EDGERS[0]) <= self.r
+        if wall_l and self.velocity[0] < 0 or wall_r and self.velocity[0] > 0:
             self.velocity[0] *= -1
 
-        if abs(self.coords[1] - 0) <= self.r  and self.velocity[1] < 0 or (
-            abs(self.coords[1] - BORDER_EDGERS[1]) <= self.r and  self.velocity[1] > 0):
+        wall_u = abs(self.coords[1] - 0) <= self.r
+        wall_d = abs(self.coords[1] - BORDER_EDGERS[1]) <= self.r
+        if wall_u and self.velocity[1] < 0 or wall_d and self.velocity[1] > 0:
             self.velocity[1] *= -1
 
     def drawing(self, surface):
@@ -52,15 +59,18 @@ class Ball:
 
     def reaction(self, event):
         '''
-        reaction to some actions
+        reaction to some mouse actions
         '''
-        if self.life and (
-                event.type == pygame.MOUSEBUTTONDOWN) and event.button == 1 and (
-                event.pos[0] - self.coords[0]) ** 2 + (
-                event.pos[1] - self.coords[1]) ** 2 <= self.r ** 2:
+        type_mouse = event.type == pygame.MOUSEBUTTONDOWN
+        if type_mouse:
+            mouse_ball_x = (event.pos[0] - self.coords[0]) ** 2
+            mouse_ball_y = (event.pos[1] - self.coords[1]) ** 2
+            hit = mouse_ball_x + mouse_ball_y <= self.r ** 2
+        if self.life and type_mouse and event.button == 1 and hit:
             self.life = False
             return (10 // self.r) + (self.velocity[0] ** 2 +
-                                     self.velocity[1] ** 2) ** 0.5 / 5 + 1  # score for this event
+                                     self.velocity[1] ** 2) ** 0.5 / 5 + 1
+        # score for this event
         else:
             return 0
 
@@ -68,54 +78,141 @@ class Ball:
 class Ellipse:
     def __init__(self):
         '''
-        init method to class Ball
+        init method to class Ellipse
         '''
         max_velocity = 5
         self.coords = [random.randint(0, 600), random.randint(0, 600)]
+        # coordinates of left corner
         self.color = (random.randint(0, 255), random.randint(0, 255),
                       random.randint(0, 255))
         self.velocity = [random.randint(-max_velocity, max_velocity),
                          random.randint(-max_velocity, max_velocity)]
+        # velocity
         self.size = (random.randint(10, 100), random.randint(10, 100))
+        # weight and height of rectangle over ellipse
         self.life = True
 
     def move(self):
         '''
-        move ball
+        move ellipse
         '''
         self.coords = [self.coords[0] + self.velocity[0],
                        self.coords[1] + self.velocity[1]]
+        # change coordinates
         self.reflection(pygame.display.get_surface().get_size())
 
     def reflection(self, BORDER_EDGERS):
         '''
         reflection from wall
         '''
-        if self.coords[0] <= 0 and self.velocity[0] < 0 or (
-            abs(self.coords[0] - BORDER_EDGERS[0]) <= self.size[0] and self.velocity[0] > 0):
+        ellipse_wall_r = abs(self.coords[0] - BORDER_EDGERS[0]) <= self.size[0]
+        ellipse_wall_l = self.coords[0] <= 0
+        if ellipse_wall_l and self.velocity[0] < 0 or (
+           ellipse_wall_r and self.velocity[0] > 0):
             self.velocity[0] *= -1
 
-        if self.coords[1] <= 0 and self.velocity[1] < 0 or (
-            abs(self.coords[1] - BORDER_EDGERS[1]) <= self.size[1] and self.velocity[1] > 0):
+        ellipse_wall_u = abs(self.coords[1] - BORDER_EDGERS[1]) <= self.size[1]
+        ellipse_wall_d = self.coords[1] <= 0
+        if ellipse_wall_d and self.velocity[1] < 0 or (
+           ellipse_wall_u and self.velocity[1] > 0):
             self.velocity[1] *= -1
 
     def drawing(self, surface):
         '''
-        draw ball
+        draw ellipse
         '''
         if self.life:
-            pygame.draw.ellipse(screen, self.color, tuple([self.coords, self.size]))
+            pygame.draw.ellipse(screen, self.color,
+                                tuple([self.coords, self.size]))
 
     def reaction(self, event):
         '''
-        reaction to some actions
+        reaction to some mouse actions
         '''
-        if self.life and event.type == pygame.MOUSEBUTTONDOWN and (
-                event.button == 1) and abs(
-            event.pos[0] - self.coords[0] - self.size[0] / 2) <= self.size[0] / 2 and abs(
-            event.pos[1] - self.coords[1] - self.size[1] / 2) ** 2 <= self.size[1] / 2:
+        type_mouse = event.type == pygame.MOUSEBUTTONDOWN
+        if type_mouse:
+            mouse_ellipse_x = event.pos[0] - self.coords[0] - self.size[0] / 2
+            hit_x = abs(mouse_ellipse_x) <= self.size[0] / 2
+            mouse_ellipse_y = event.pos[1] - self.coords[1] - self.size[1] / 2
+            hit_y = abs(mouse_ellipse_y) <= self.size[1] / 2
+
+        if self.life and type_mouse and event.button == 1 and hit_x and hit_y:
             self.life = False
-            return 1 + (self.velocity[0] ** 2 + self.velocity[1] ** 2) ** 0.5 / 5 + 1  # score for this event
+            score = (self.velocity[0] ** 2 + self.velocity[1] ** 2) ** 0.5 / 5
+            return 1 + score
+        # score for this event
+        else:
+            return 0
+
+
+class Lama:
+    def __init__(self):
+        '''
+        init method to class Lama
+        '''
+        max_velocity = 5
+        self.color = (random.randint(0, 255), random.randint(0, 255),
+                      random.randint(0, 255))
+        self.velocity = [random.randint(-max_velocity * 10,
+                                        max_velocity * 10) / 10, 0]
+        self.size = random.randrange(4, 10) / 10
+        # size of lama
+        lama_rect = pygame.image.load('Lama.png').get_rect()
+        self.size_rect = [self.size * lama_rect.width,
+                          self.size * lama_rect.height]
+        # lama's rectangle
+        self.coords = (random.randrange(0, display_size[0]),
+                       random.randrange(int(self.size_rect[1]),
+                       int(int(display_size[1] - 0.5 * self.size_rect[1]))))
+        # lama's coordinates
+        self.life = True
+
+    def move(self):
+        '''
+        move lama
+        '''
+        self.coords = [self.coords[0] + self.velocity[0],
+                       self.coords[1] + self.velocity[1]]
+        # change coordinates
+        self.reflection(pygame.display.get_surface().get_size())
+
+    def reflection(self, BORDER_EDGERS):
+        '''
+        reflection from wall
+        '''
+        wall_l = (self.coords[0] - self.size_rect[0]) <= 0
+        wall_r = abs(self.coords[0] - BORDER_EDGERS[0]) <= self.size_rect[0]
+        if wall_l and self.velocity[0] < 0 or wall_r and self.velocity[0] > 0:
+            self.velocity[0] *= -1
+
+        wall_u = (self.coords[1] - self.size_rect[1]) <= 0
+        wall_d = abs(self.coords[1] - BORDER_EDGERS[1]) <= self.size_rect[1]
+        if wall_u and self.velocity[1] < 0 or wall_d and self.velocity[1] > 0:
+            self.velocity[1] *= -1
+
+    def drawing(self, surface):
+        '''
+        draw lama
+        '''
+        if self.life:
+            lama_surface = pygame.image.load('Lama.png')
+            if self.velocity[0] < 0:
+                lama_surface = pygame.transform.flip(lama_surface, True, False)
+            lama_surface = pygame.transform.rotozoom(lama_surface, 0, self.size)
+            lama_surface.set_colorkey((175, 221, 233))
+            surface.blit(lama_surface, lama_surface.get_rect(center=self.coords))
+
+    def reaction(self, event):
+        '''
+        reaction to some mouse actions
+        '''
+        mouse_type = event.type == pygame.MOUSEBUTTONDOWN
+        if mouse_type:
+            hit_x = abs(event.pos[0] - self.coords[0]) <= self.size_rect[0] / 2
+            hit_y = abs(event.pos[1] - self.coords[1]) <= self.size_rect[1] / 2
+        if self.life and mouse_type and event.button == 1 and hit_x and hit_y:
+            self.life = False
+            return -2  # score for this event
         else:
             return 0
 
@@ -132,16 +229,46 @@ def write_text(text, surface, pos):
     surface.blit(text_surface, pos)
 
 
+def write_table(array, surface, pos):
+    '''
+    write table on some surface
+    text - wanting text
+    surface - wanting surface
+    pos - (x, y) - coordinates of left corner
+    '''
+    font = pygame.font.Font(None, 36)
+    line_size = [0 for _ in array[0]]
+    # array of size of columns of table
+    for i in array:
+        for j in i:
+            if font.size(str(j))[0] > line_size[i.index(j)]:
+                line_size[i.index(j)] = font.size(str(j))[0]
+    line_size = [i + font.size('   ')[0] for i in line_size]
+
+    y_of_line = pos[1]
+    line_delta = 0
+    for i in array:
+        for j in i:
+            write_text(str(j), surface, (pos[0] + line_delta, y_of_line))
+            line_delta += line_size[i.index(j)]
+        y_of_line += 30
+        line_delta = 0
+    # print table on the surface
+
 pool = []
-n = random.randint(2, 30)
-number_of_existing = n
+n = random.randint(2, 40)
+number_of_objects = n
+number_of_lamas = 0
 for _ in range(n):
-    k = random.randint(0, 2)
-    if k == 0:
+    k = random.randint(0, 3)
+    if k % 3 == 0:
         pool.append(Ball())
-    else:
+    elif k % 3 == 1:
         pool.append(Ellipse())
-# making pool of objects class Ball and Ellipse
+    else:
+        pool.append(Lama())
+        number_of_lamas += 1
+# making pool of objects class Ball, Ellipse and Lama
 
 score = 0
 pygame.display.update()
@@ -150,8 +277,10 @@ finished = False
 
 name = ''
 enter_name = False
-display_size_x = pygame.display.get_surface().get_size()[0]
-display_size_y = pygame.display.get_surface().get_size()[1]
+
+# take information about previous games
+with open('Results.yaml') as file:
+    data = yaml.load(file)
 
 while not finished:
     clock.tick(FPS)
@@ -161,46 +290,62 @@ while not finished:
             # exit in game
         if event.type == pygame.KEYDOWN:
             # expectation of player's name
-            if event.key == pygame.K_KP_ENTER:
+            if event.key == pygame.K_RETURN or event.key == pygame.K_KP_ENTER:
                 enter_name = True
+            elif event.key == pygame.K_BACKSPACE:
+                name = name[0:-1]
             else:
                 name += event.unicode
         else:
             # react for some action of mouse
             for i in range(n):
                 reaction = pool[i].reaction(event)
-                if reaction > 0:
-                    number_of_existing -= 1
+                if reaction != 0:
+                    number_of_objects -= 1
+                if reaction < 0:
+                    number_of_lamas -= 1
                 score += int(reaction)
 
     if enter_name:
-        for i in range(n):
-            pool[i].move()
-            pool[i].drawing(screen)
-            # draw objects from pool
-
-        if number_of_existing == 0:
+        if number_of_objects <= number_of_lamas:
+            # final of the game
             write_text('Good job, ' + name + '!', screen,
-                       (display_size_x / 2 - 100, display_size_y / 2 - 100))
+                       (display_size[0] / 2, display_size[1] / 3))
             write_text('Score: ' + str(score), screen,
-                       (display_size_x / 2 - 100, display_size_y / 2))
+                       (display_size[0] / 2, display_size[1] / 3 + 40))
+            # write motivation text
+
+            data['results'].append({'score': score, 'name': name})
+            scores = sorted([[i['score'], i['name']] for i in data['results']])
+            scores.reverse()
+            scores = [[j[1], j[0]] for j in scores]
+            y_of_line = display_size[1] / 3
+            write_text('Table of less losers of game:', screen, (20, y_of_line))
+            write_table(scores[0:5], screen, (20, y_of_line + 50))
+            # print table with best results
 
         else:
+            for i in range(n):
+                pool[i].move()
+                pool[i].drawing(screen)
+                # draw objects from pool
+
             write_text('Score: ' + str(score), screen,
-                       (display_size_x - 160, display_size_y - 100))
+                       (display_size[0] - 160, display_size[1] - 100))
         # write text about score
 
     else:
         write_text('Enter your name: ', screen,
-                   (display_size_x / 3, display_size_y / 3))
-        write_text(name, screen, (display_size_x / 3, display_size_y / 2))
+                   (display_size[0] / 3, display_size[1] / 3))
+        write_text(name, screen, (display_size[0] / 3, display_size[1] / 2))
+        write_text("Don't touch lamas!", screen,
+                   (display_size[0] / 3, 2 * display_size[1] / 3))
         # expectation of writing player's name
 
     pygame.display.update()
     screen.fill(screen_color)
 
 # write score information in file
-file = open('Results.txt', 'a')
-print(name, score, file=file)
-file.close()
+with open('Results.yaml', 'w') as file:
+    yaml.dump(data, file)
 pygame.quit()
